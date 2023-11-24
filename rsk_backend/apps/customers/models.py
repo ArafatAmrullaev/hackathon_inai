fd4645ab9aa329838a82cac2a9848d344b0cb2dd
Arafat <arafatamrullaev021@gmail.com>
2023-11-25 00:06:38 +0600

    4

--- rsk_backend/apps/customers/models.py
+++ rsk_backend/apps/customers/models.py
@@ -1,8 +1,16 @@
+import os
+
 from django.db import models
+
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    new_filename = f"{instance.passport_id}.{ext}"
+    return os.path.join("customers", new_filename)
+
 
 class Customer(models.Model):
     passport_id = models.CharField(max_length=9, primary_key=True)
-    pic = models.ImageField(upload_to='customers')
+    pic = models.ImageField(upload_to=upload_to)
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50)
@@ -12,5 +20,8 @@ class Customer(models.Model):
     id_expire_date = models.DateField()
     id_recieved = models.DateField()
     vip_status = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
     
 
